4276a23b30bf22cce79aa975a315647ace291070
Ueverton Passos <uevertonpassos96@gmail.com>
2023-02-28 16:18:08 -0300

    uw

---
+++ exercicios/main.java
@@ -0,0 +1,10 @@
+package alunos;
+
+public class AlunosMatriculados {
+    public AlunosMatriculados() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("registro de alunos matriculados");
+    }
+}
